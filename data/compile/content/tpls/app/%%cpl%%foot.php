4c00b2a38ac1c2f0e12998386ae4fbce563f9b63
cht <476716867@qq.com>
2015-08-19 16:42:39 +0800

    “文字修改v15

--- data/compile/content/tpls/app/%%cpl%%foot.php
+++ data/compile/content/tpls/app/%%cpl%%foot.php
@@ -1,7 +1,7 @@
 <!-- footer-bottom -->
 	<div class="footer-bottom">
 		<div class="container">
-			<p align="center">Copyright &copy; 2015<a target="_blank" href="http://www.bupt.edu.cn/">全国大学生移动互联网创新大赛组委会</a></p>
+			<p align="center">Copyright &copy; 2016<a target="_blank" href="http://www.bupt.edu.cn/">全国大学生移动互联网创新大赛组委会</a></p>
 		</div>
 	</div>
 <!-- footer-bottom -->
--- data/compile/content/tpls/app/%%cpl%%index.php
+++ data/compile/content/tpls/app/%%cpl%%index.php
@@ -31,7 +31,7 @@
 		<div class="container">
 			<div class="news-text">
 			    <h5> &nbsp </h5>
-				<h3>&nbsp 组委会构成</h3>
+				<h3>&nbsp 考场链接</h3>
 			</div>
 			<div class="news-grids">
 			    <?php $hid = 0;
@@ -55,7 +55,11 @@
 				    <img src="app/core/styles/css/img/new/img2.jpg" alt="" />
 					<h4><strong>PPC</strong></h4>
 				  </a>
-				</div>
+			</div>
+						<div class="news-text">
+			    <h5> &nbsp </h5>
+				<h3>&nbsp 组委会构成</h3>
+			</div>
 				<div class="col-md-3 news-grid wow fadeInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
 				  <a href="index.php">
 				    <img src="app/core/styles/css/img/new/img3.jpg" alt="" />
@@ -74,12 +78,6 @@
 					<h4><strong>陈豪庭</strong></h4>
 				  </a>
 			    </div>
-				<div class="col-md-3 news-grid wow fadeInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
-				  <a href="index.php">
-				    <img src="app/core/styles/css/img/new/img7.jpg" alt="" />
-					<h4><strong>STEGA</strong></h4>
-				  </a>
-				</div>
 				<div class="clearfix"> </div>
 			</div>
 		</div>
