0c0ad127977c6fd74707c5b79ddcce0ca336ce2a
cht <476716867@qq.com>
2016-07-12 13:41:53 +0800

    test v1

--- hide.php
+++ hide.php
@@ -19,11 +19,28 @@
 			$this->db_score="ehscore";
 			$this->db_time="ehtime";
 			$this->db_comp="user_company";
-			$dbadd="10.109.35.214";
+/*			$dbadd="10.109.35.214";
 			$dbun="root";
 			$dbp="1qaZ@wsx)p;/";
-			$dbn="pechina";
-			$conn = @mysql_connect($dbadd,$dbun,$dbp);
+			$dbn="pechina";*/
+      $dbadd="w.rdc.sae.sina.com.cn";
+      $dbun="4k003x4xwx";
+      $dbp="2ymmhmjzx2m02jw3m5hzw3xjim03zz2420mljy1m";
+      $dbn="app_testcht";
+      $SAE_MYSQL_PORT = "3307";
+/*            $dbadd="localhost";
+      $dbun="root";
+      $dbp="root";
+      $dbn="pechina";
+      $SAE_MYSQL_PORT = "3306";*/
+/*      用户名　 :  SAE_MYSQL_USER
+密　　码 :  SAE_MYSQL_PASS
+主库域名 :  SAE_MYSQL_HOST_M
+从库域名 :  SAE_MYSQL_HOST_S
+端　　口 :  SAE_MYSQL_PORT
+数据库名 :  SAE_MYSQL_DB*/
+			/*$conn = @mysql_connect($dbadd,$dbun,$dbp);*/
+      $conn = mysql_connect($dbadd.':'.$SAE_MYSQL_PORT,$dbun,$dbp);
 			if (!$conn){
                die("连接数据库失败：" . mysql_error());
             }
