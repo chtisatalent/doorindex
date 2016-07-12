a609d4bcf90cccf7bc76caef43e9c5b797608389
cht <476716867@qq.com>
2016-07-12 20:57:44 +0800

    version 3

--- app/document/config/config.php
+++ app/document/config/config.php
@@ -10,10 +10,16 @@ define('PN',20);
 define('TIME',time());
 define('WP','http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']));
 
-define('DB','exam');
+/*define('DB','exam');
 define('DH','localhost');
 define('DU','root');
 define('DP','root');
-define('DTH','x2_');
+define('DTH','x2_');*/
+define('DB','app_doorblog');//MYSQL数据库名
+define('DH','w.rdc.sae.sina.com.cn:3307');//MYSQL主机名，不用改
+define('DU','ym3l0kmj01');//MYSQL数据库用户名
+define('DP','z5h5mikk011i1wmymy402zhzk05wijy2l0hx1m52');//MYSQL数据库用户密码
+define('DTH','x2_');//系统表前缀，不用改
+define('PT','3307');//系统表前缀，不用改
 
 ?>
